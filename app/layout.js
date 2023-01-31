f3aef21cb23c6c6e638bf8ce433f580e11918920
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-01-31 04:49:31 +0100

    added chakra ui

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
+"use client"
 import './globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function RootLayout({children}) {
   return (
     <html lang="en">
     <head/>
     <body>
+    <ChakraProvider>
       {children}
+    </ChakraProvider>
     </body>
     </html>
   )
